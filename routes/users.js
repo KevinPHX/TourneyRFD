87cfc85c1711c4adf9b03ac7c2ad26b006fba206
KevinPHX <30714010+KevinPHX@users.noreply.github.com>
2018-02-13 17:36:05 -0700

    more stuff

--- routes/users.js
+++ routes/users.js
@@ -20,7 +20,7 @@ mongoose.connect(config.database);
 
 
 nev.configure({
-    verificationURL: 'http://localhost:3000/users/email-verification/${URL}',
+    verificationURL: 'http://brophycarpool.com/users/email-verification/${URL}',
     persistentUserModel: User,
     tempUserCollection: 'temporary_users',
 
@@ -376,7 +376,7 @@ console.log('step 1')
         subject: 'Brophy Carpool Password Reset',
         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'http://localhost:4200/reset/' + token + '\n\n' +
+          'http://brophycarpool.com/reset/' + token + '\n\n' +
           'If you did not request this, please ignore this email and your password will remain unchanged.\n'
 
       };
